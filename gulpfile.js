24160857bfbc998c28398401938bfca65afcfd11
hustlzp <hustlzp@qq.com>
2014-04-20 22:27:34 +0800

    split task build to 2 tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,31 @@ var html_file = 'index.html';
 var jade_file = 'index.jade';
 
 gulp.task('build', function () {
-  gulp.watch([jade_file, data_file], {}, function () {
-    // 重新载入data.js
-    delete require.cache[require.resolve('./' + data_file)];
-    var data = require('./' + data_file);
+  // 重新载入data.js
+  delete require.cache[require.resolve('./' + data_file)];
+  var data = require('./' + data_file);
 
-    jade.renderFile(jade_file, data, function (err, html) {
+  jade.renderFile(jade_file, data, function (err, html) {
+    if (err) {
+      console.log(new Date() + ' - ERROR');
+      fs.writeFile(html_file, err);
+      return;
+    }
+
+    fs.writeFile(html_file, html, function (err) {
       if (err) {
         console.log(new Date() + ' - ERROR');
         fs.writeFile(html_file, err);
         return;
       }
 
-      fs.writeFile(html_file, html, function (err) {
-        if (err) {
-          console.log(new Date() + ' - ERROR');
-          fs.writeFile(html_file, err);
-          return;
-        }
-
-        console.log(new Date() + ' - SAVED');
-      });
+      console.log(new Date() + ' - SAVED');
     });
   });
 });
 
-gulp.task('default', ['build']);
+gulp.task('watch', function () {
+  gulp.watch([jade_file, data_file], ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
