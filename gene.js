f4da0bec0a3e0045ec2720db92d3b254d23d60a1
hustlzp <hustlzp@qq.com>
2014-04-20 15:19:27 +0800

    add package.json

--- gene.js
+++ gene.js
@@ -4,7 +4,7 @@ var chokidar = require('chokidar');
 var data = require('./data.js');
 
 var watcher = chokidar.watch(['index.jade', 'data.js'], {persistent: true});
-console.log('Start watching file...');
+console.log('Start watching...');
 
 watcher.on('change', function (path) {
   // 重新导入data.js
@@ -13,15 +13,15 @@ watcher.on('change', function (path) {
 
   jade.renderFile('index.jade', data, function (err, html) {
     if (err) {
-      console.log(new Date() + ' ERROR');
+      console.log(new Date() + ' - ERROR');
       fs.writeFile('index.html', err);
     }
 
     fs.writeFile('index.html', html, function (err) {
-      console.log(new Date() + ' SAVED');
+      console.log(new Date() + ' - SAVED');
 
       if (err) {
-        console.log(new Date() + ' ERROR');
+        console.log(new Date() + ' - ERROR');
         fs.writeFile('index.html', err);
       }
     });
