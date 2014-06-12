6bf0d88d9a69cf83ddc1d7fe91185ce139d1cf38
hustlzp <hustlzp@qq.com>
2014-06-12 20:00:34 +0800

    use bluebird

--- gulpfile.js
+++ gulpfile.js
@@ -3,42 +3,34 @@ var jade = require('jade');
 var gulp = require('gulp');
 var yaml = require('js-yaml');
 var moment = require('moment');
+var Promise = require("bluebird");
 
 var dataFile = 'data.yml';
 var jadeFile = 'index.jade';
 var htmlFile = 'index.html';
 
+// 将相关函数promisify
+Promise.promisifyAll(fs);
+var renderFile = Promise.promisify(jade.renderFile);
+
 // 将data.yml和index.jade编译为index.html
 gulp.task('build', function () {
-  var data;
-
-  try {
-    data = yaml.safeLoad(fs.readFileSync(dataFile, 'utf8'));
-  } catch (e) {
-    logError(e);
-    return;
-  }
-
-  // 输出未压缩的HTML
-  data.pretty = true;
-  data.updateTime = moment().format("YYYY-MM-DD");
-  data.currentYear = moment().format("YYYY");
-
-  jade.renderFile(jadeFile, data, function (err, html) {
-    if (err) {
-      logError(err);
-      return;
-    }
-
-    fs.writeFile(htmlFile, html, function (err) {
-      if (err) {
-        logError(err);
-        return;
-      }
-
+  fs.readFileAsync(dataFile, "utf8").then(function (content) {
+    return yaml.safeLoad(fs.readFileSync(dataFile, 'utf8'));
+  }).then(function (data) {
+      // 输出未压缩的HTML
+      data.pretty = true;
+      data.updateTime = moment().format("YYYY-MM-DD");
+      data.currentYear = moment().format("YYYY");
+      return renderFile(jadeFile, data);
+    }).then(function (html) {
+      return fs.writeFileAsync(htmlFile, html);
+    }).then(function () {
       console.log(moment().format("YYYY-MM-DD HH:mm:ss") + ' - SAVED');
+    }).catch(function (err) {
+      console.log(moment().format("YYYY-MM-DD HH:mm:ss") + ' - ERROR');
+      fs.writeFile(htmlFile, err);
     });
-  });
 });
 
 // 源文件变动时，触发编译
@@ -48,11 +40,3 @@ gulp.task('watch', function () {
 
 // gulp运行时触发一次编译，然后开启监控进程
 gulp.task('default', ['build', 'watch']);
-
-/**
- * 将错误信息记录到htmlFile，以方便调试
- */
-function logError(err) {
-  console.log(moment().format("YYYY-MM-DD HH:mm:ss") + ' - ERROR');
-  fs.writeFile(htmlFile, err);
-}
